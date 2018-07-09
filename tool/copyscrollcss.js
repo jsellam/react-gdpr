26796d3860fccc082155e3abb2782bb93e5c4320
Jérémie Sellam <jsellam@big-boss-studio.com>
2018-07-09 18:29:47 +0200

    fix css generation

---
+++ tool/copyscrollcss.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+
+fs.copyFileSync('./node_modules/overlayscrollbars/css/OverlayScrollbars.css', './sass/OverlayScrollbars.scss');
+
--- tool/sass.js
+++
@@ -1,5 +0,0 @@
-var sass = require('node-sass');
-sass.render({
-  file: './sass/GDPRCustom.scss',
-  outFile: './css/index.css',
-}, function(err, result) { console.log(err,result) });
