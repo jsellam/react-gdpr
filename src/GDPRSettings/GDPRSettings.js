84085543324b1d1116d81a1655d83709ea12646c
Cris Ryan Tan <crisryantan@gmail.com>
2018-09-14 12:33:28 +0800

    add cancel btn
    - updated README.md
    - updated build
    - align code properly

--- src/GDPRSettings/GDPRSettings.js
+++ src/GDPRSettings/GDPRSettings.js
@@ -1,85 +1,101 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'class-names'
-import GDPRSection from './partials/GDPRSection'
-import OverlayScrollbars from 'overlayscrollbars'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'class-names';
+import GDPRSection from './partials/GDPRSection';
+import OverlayScrollbars from 'overlayscrollbars';
 //import 'overlayscrollbars/css/OverlayScrollbars.css'
 
-
-
-
 export default class GDPRSettings extends Component {
-    static propTypes = {
-        className: PropTypes.string,
-        content:PropTypes.arrayOf(PropTypes.shape({
-            id:PropTypes.any.isRequired,
-            title:PropTypes.string.isRequired,
-            description:PropTypes.string,
-            isAllowed:PropTypes.bool.isRequired,
-            allowChange:PropTypes.bool
-        })).isRequired,
-        locale:PropTypes.shape({
-           gdpr_title:PropTypes.string,
-           gdpr_description:PropTypes.string,
-           gdpr_validate:PropTypes.string,
-           gdpr_active:PropTypes.string,
-           gdpr_inactive:PropTypes.string,
-           gdpr_always_active:PropTypes.string
-        }).isRequired,
-        onToggle:PropTypes.func,
-        onValidate:PropTypes.func
+  static propTypes = {
+    className: PropTypes.string,
+    content: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.any.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        isAllowed: PropTypes.bool.isRequired,
+        allowChange: PropTypes.bool
+      })
+    ).isRequired,
+    locale: PropTypes.shape({
+      gdpr_title: PropTypes.string,
+      gdpr_description: PropTypes.string,
+      gdpr_validate: PropTypes.string,
+      gdpr_active: PropTypes.string,
+      gdpr_inactive: PropTypes.string,
+      gdpr_always_active: PropTypes.string,
+      gdpr_cancel: PropTypes.string
+    }).isRequired,
+    onToggle: PropTypes.func,
+    onValidate: PropTypes.func,
+    onCancel: PropTypes.func
+  };
 
-    }
+  static defaultProps = {
+    className: '',
+    onToggle: (id, value) => {},
+    onValidate: () => {},
+    onCancel: () => {}
+  };
 
-    static defaultProps = {
-        className:'',
-        onToggle:(id,value)=>{},
-        onValidate:()=>{}
-    }
+  componentDidMount() {
+    new OverlayScrollbars(document.querySelector('.gdpr-content-wrapper'), {
+      sizeAutoCapable: false,
+      className: 'os-theme-dark'
+    });
+  }
 
+  handleToggle = (id, value) => {
+    this.props.onToggle(id, value);
+  };
 
-    componentDidMount()
-    {
-       new  OverlayScrollbars(document.querySelector('.gdpr-content-wrapper'), { sizeAutoCapable:false,className : "os-theme-dark"});
-        
-          
-    }
+  handleClickValidate = () => {
+    this.props.onValidate();
+  };
 
+  handleClickCancel = () => {
+    this.props.onCancel();
+  };
 
-
-
-    handleToggle = (id,value)=>{
-        this.props.onToggle(id,value)
-    }
-
-    handleClickValidate = () =>{
-        this.props.onValidate()
-    }
-
-
-    render() {
-        return (
-        <div className={classNames('gdpr-settings',this.props.className)}>
-            <div className="gdpr-overlay" />
-            <div className="gdpr-popin">
-                <div className="gdpr-container">
-                    <div className="gdpr-title">{this.props.locale.gdpr_title}</div>
-                    <div className="gdpr-content-wrapper os-host-flexbox">
-                   
-                    <div className="gdpr-content">
-                        <div className="gdpr-description">{this.props.locale.gdpr_description}</div>
-                        {
-                            this.props.content.map(props =><GDPRSection onToggle={this.handleToggle} key={props.id} {...props} locale={this.props.locale} />)
-                        }
-                    </div>
-                  
-                    </div>
-                    <div className="gdpr-footer">
-                        <div onClick={this.handleClickValidate} className="gdpr-button gdpr-button-accept">{this.props.locale.gdpr_validate}</div>
-                    </div>
+  render() {
+    return (
+      <div className={classNames('gdpr-settings', this.props.className)}>
+        <div className="gdpr-overlay" />
+        <div className="gdpr-popin">
+          <div className="gdpr-container">
+            <div className="gdpr-title">{this.props.locale.gdpr_title}</div>
+            <div className="gdpr-content-wrapper os-host-flexbox">
+              <div className="gdpr-content">
+                <div className="gdpr-description">
+                  {this.props.locale.gdpr_description}
                 </div>
+                {this.props.content.map(props => (
+                  <GDPRSection
+                    onToggle={this.handleToggle}
+                    key={props.id}
+                    {...props}
+                    locale={this.props.locale}
+                  />
+                ))}
+              </div>
             </div>
+            <div className="gdpr-footer">
+              <div
+                onClick={this.handleClickValidate}
+                className="gdpr-button gdpr-button-accept"
+              >
+                {this.props.locale.gdpr_validate}
+              </div>
+              <div
+                onClick={this.handleClickCancel}
+                className="gdpr-button gdpr-button-cancel"
+              >
+                {this.props.locale.gdpr_cancel}
+              </div>
+            </div>
+          </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
--- src/GDPRSettings/partials/GDPRSection.js
+++ src/GDPRSettings/partials/GDPRSection.js
@@ -1,44 +1,49 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'class-names'
-import GDPRToggle from './GDPRToggle'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'class-names';
+import GDPRToggle from './GDPRToggle';
 
 export default class GDPRSection extends Component {
-    static propTypes = {
-        className: PropTypes.string,
-        id:PropTypes.any.isRequired,
-        title:PropTypes.string.isRequired,
-        description:PropTypes.string,
-        isAllowed:PropTypes.bool.isRequired,
-        allowChange:PropTypes.bool,
-        locale:PropTypes.shape({
-            gdpr_active:PropTypes.string,
-           gdpr_inactive:PropTypes.string,
-           gdpr_always_active:PropTypes.string
-        }).isRequired,
-        onToggle:PropTypes.func
-    }
+  static propTypes = {
+    className: PropTypes.string,
+    id: PropTypes.any.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    isAllowed: PropTypes.bool.isRequired,
+    allowChange: PropTypes.bool,
+    locale: PropTypes.shape({
+      gdpr_active: PropTypes.string,
+      gdpr_inactive: PropTypes.string,
+      gdpr_always_active: PropTypes.string
+    }).isRequired,
+    onToggle: PropTypes.func
+  };
 
-    static defaultProps = {
-        className:'',
-        description:'',
-        allowChange:true,
-        onToggle:(id,value)=>{}
-    }
+  static defaultProps = {
+    className: '',
+    description: '',
+    allowChange: true,
+    onToggle: (id, value) => {}
+  };
 
-    handleToggle = (value) =>{
-        this.props.onToggle(this.props.id,value)
-    }
-  
+  handleToggle = value => {
+    this.props.onToggle(this.props.id, value);
+  };
 
-
-    render() {
-        return (
-        <div className={classNames('gdpr-section',this.props.className)}>
-            <div className="gdpr-title gdpr-section-title">{this.props.title}</div>
-            <div className="gdpr-description gdpr-section-description">{this.props.description}</div>
-            <GDPRToggle onToggle={this.handleToggle} allowChange={this.props.allowChange} isActive={this.props.isAllowed} locale={this.props.locale} />
+  render() {
+    return (
+      <div className={classNames('gdpr-section', this.props.className)}>
+        <div className="gdpr-title gdpr-section-title">{this.props.title}</div>
+        <div className="gdpr-description gdpr-section-description">
+          {this.props.description}
         </div>
-        )
-    }
+        <GDPRToggle
+          onToggle={this.handleToggle}
+          allowChange={this.props.allowChange}
+          isActive={this.props.isAllowed}
+          locale={this.props.locale}
+        />
+      </div>
+    );
+  }
 }
